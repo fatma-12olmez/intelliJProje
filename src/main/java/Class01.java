9e77da3e7754337d2dfc5f465e7bafe862e2744b
fatma-12olmez <olmezfatma94@gmail.com>
2023-02-25 23:36:56 +0100

    v8

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -4,7 +4,8 @@ public class Class01 {
     public static void main(String[] args) {
         System.out.println("1.calismam...");
         System.out.println("v3");
-        System.out.println("Bugun hava yagisli");
+       //Master kodu
+
 
     }
 }
