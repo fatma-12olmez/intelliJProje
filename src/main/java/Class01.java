61bfc31bf9e4ea3e73390ad554c08031b83d001c
fatma-12olmez <olmezfatma94@gmail.com>
2023-02-25 23:24:23 +0100

    v5

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -4,5 +4,7 @@ public class Class01 {
     public static void main(String[] args) {
         System.out.println("1.calismam...");
         System.out.println("v3");
+        System.out.println("Bugun hava yagisli");
+
     }
 }
